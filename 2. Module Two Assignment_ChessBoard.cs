a4ee4990ec308ca78dbaf8be9e4b788078f3b3e7
Narendra Singh <narendrasingh@outlook.in>
2015-05-14 22:54:57 +0530

    Module Two Assignment
    For this assignment, you will create the pattern of a chess board that
    is 8 x 8.  Use X and O to represent the squares.
    
    Create the appropriate nested looping structure to output the characters
    in an 8 x 8 grid on the screen using Console.Write() or
    Console.WriteLine() as appropriate.
    Include a decision structure to ensure that alternate rows start with
    opposite characters as a real chess board alternates the colors among
    rows.
    This is what your output should look like.
    
    XOXOXOXO
    OXOXOXOX
    XOXOXOXO
    OXOXOXOX
    XOXOXOXO
    OXOXOXOX
    XOXOXOXO
    OXOXOXOX

---
+++ 2. Module Two Assignment_ChessBoard.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var grid = CreateGrid(8, 'o', 'x');
+            PrintGrid(grid);
+        }
+
+        private static void PrintGrid(char[,] grid)
+        {
+            for (var row = 0; row < grid.GetLength(0); row++)
+            {
+                for (var col = 0; col < grid.GetLength(0); col++)
+                {
+                    Console.Write(grid[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static char[,] CreateGrid(int gridSize, char light, char dark)
+        {
+            var grid = new char[gridSize, gridSize];
+
+            for (var row = 0; row < gridSize; row++)
+            {
+                for (var col = 0; col < gridSize; col++)
+                {
+                    if (row % 2 == 0)
+                    {
+                        grid[row, col] = (col % 2 == 0) ? dark : light;
+                    }
+                    else
+                    {
+                        grid[row, col] = (col % 2 == 0) ? light : dark;
+                    }
+                }
+            }
+
+            return grid;
+            Console.ReadLine();
+        }
+    }
+}
